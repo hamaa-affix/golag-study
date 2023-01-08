41ae4efb50e0214153a803f5755966dfc34a66ec
kenta <kenta@kentanoMacBook-Air.local>
2023-01-08 23:36:38 +0900

    制御構文

--- main.go
+++ main.go
@@ -3,29 +3,29 @@ package main
 import (
 	"errors"
 	"fmt"
+	// "log"
 )
+var slice = []string{"Golang", "Ruby", "Javascript", "Python"}
 
 func main() {
     // コンソールにメッセージが出力される
 	// hoge()
-	fmt.Println(Green)
-	fmt.Printf("%T\n",Green )
-
-	//3 fizz 5 bazu
-	for i := 1; i < 100; i++ {
-		if  i % 15 == 0 {
-			fmt.Println("fizz bazz")
-		} else if i % 5 == 0 {
-			fmt.Println("bazz")
-		} else if i % 3 == 0  {
-			fmt.Println("fizz")
-		} else {
-			fmt.Println(i)
-		}
+	
+	// if err := isString(32); err == nil {
+	// 	fmt.Println("文字列です")
+	// } else {
+	// 	// log.Fatal("not string")
+	// 	log.Fatal(err)
+	// }
+	
+	for _, value := range slice {
+		fmt.Println( value)
 	}
+
+
 }
 
-func isString(arg string) error {
+func isString(arg interface{}) error {
 	isString := interface{}(arg)
 
 	if _, ok := isString.(string); !ok {
