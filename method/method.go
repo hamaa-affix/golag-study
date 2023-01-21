c8e5a1b16d18189cef6ee4bc165d943c53d92036
hamaa-affix <hamaagive1108@gmail.com>
2023-01-21 23:10:11 +0900

    hoge

--- method/method.go
+++ method/method.go
@@ -6,12 +6,13 @@ import (
 func Method() {
 	//slice
 	slice := []int{1, 2, 3, 4, 5}
+
+// int型のスライスを受け取り要素の順番をリバース関数を定義してそのスライスを返却してさい
 	s2 := Reverse(slice)
 	fmt.Println(s2)
 }
 
 // int型のスライスを受け取り要素の順番をリバース関数を定義してそのスライスを返却してさい
-
 func Reverse(s []int) []int {
 	reverse := s
 	for left, right := 0, len(s) - 1; left < right; left, right = left + 1, right -1 {
@@ -20,4 +21,3 @@ func Reverse(s []int) []int {
 
 	return reverse
 }
-
