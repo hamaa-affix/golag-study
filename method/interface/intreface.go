217bb0ea332c07a247e530de484fcc38d2153e7e
hamaa-affix <hamaagive1108@gmail.com>
2023-01-29 10:52:23 +0900

    interface問題作成

---
+++ method/interface/intreface.go
@@ -0,0 +1,19 @@
+package itf
+
+import ()
+
+// user profile getterメソッドを定義しているinterfaceを定義してください(NameGetterなど)
+
+
+// User型を定義してください
+
+// 上記で作成したinterfaceをUser型で実装してください
+
+
+//methodを基底とするuser定義型を作成して、interfaceを実装してください
+
+func Itf() {
+	// 定義したinterfaceを元に初期化してください
+
+	// 初期化したinterfaceにUserが代入できるかを確認してみてください
+}
